992387715aeaeb1b43315e2ef966c98fab6ccfa8
Streptoco <81880440+Streptoco@users.noreply.github.com>
2024-04-16 10:41:30 +0300

    Added basic integration files

---
+++ entrypoint.py
@@ -0,0 +1,14 @@
+import slack_sdk as slack
+import os
+from flask import Flask
+from slackeventsapi import SlackEventAdapter as eventAdapter
+
+app = Flask(__name__)
+
+eventAdapter = eventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/slack/events', app)
+
+client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
+
+bot = client.api_call("auth.test")['user_id']
+
+app.run(host='0.0.0.0', port=5000, debug=True)
