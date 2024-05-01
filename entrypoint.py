ed5770d47371362c773da0056f62580ce40b07d4
Streptoco <81880440+Streptoco@users.noreply.github.com>
2024-05-01 18:03:00 +0300

    added basic interaction to channel

--- entrypoint.py
+++ entrypoint.py
@@ -14,6 +14,13 @@ bot = client.api_call("auth.test")['user_id']
 @eventAdapter.on('message')
 def onMessage(message):
     print(message)
+    event = message.get('event', {})
+    channel = event.get('channel')
+    user = event.get('user')
+    text = event.get('text')
+    if bot in text:
+        print("FOR ME!")
+        client.chat_postMessage(channel=channel,text="I don't have AI yet you greek goof")
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
