0a49735eae1864fa02e4cd263f0eb60f9e06aa2c
afikat123 <afikat154@gmail.com>
2024-05-26 18:52:17 +0300

    make the bot generate "only" 2 messages in a row

--- jager_common/slack_client.py
+++ jager_common/slack_client.py
@@ -6,3 +6,17 @@ class SlackClient:
     def __init__(self):
         self.slack_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
         self.bot = self.slack_client.api_call("auth.test")['user_id']
+        self.num_of_messages = 0
+
+    def send_message(self):
+        if self.num_of_messages == 0:
+            self.num_of_messages += 1
+
+    def post_sending(self):
+        self.num_of_messages -= 1
+
+    def check_if_can_send(self):
+        if self.num_of_messages == 0:
+            return True
+        else:
+            return False
--- slack_implementation/backend/messaging.py
+++ slack_implementation/backend/messaging.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import slack_sdk as slack
 import os
@@ -18,45 +19,55 @@ client = SlackClient()
 # local ollama instance
 chatUrl = 'http://localhost:11434/api/chat'
 generateUrl = 'http://localhost:11434/api/generate'
-
-
+max_responses = 1
 
 @eventAdapter.on('message')
 def onMessage(message):
-    # print(message)
     event = message.get('event', {})
     channel = event.get('channel')
     text = event.get('text')
-    print("Bot GUID:" + client.bot)
-    print("Username: " + messaging_parser.get_real_name(message, client))
-    print("Message: " + text)
-    print(message)
-    if "@"+client.bot in text:
-        #botMessage = client.slack_client.chat_postMessage(channel=channel,text="I'm Thinking...")
-        '''
-        body = {
-            "model": "llama3",
-            "messages": [
-                {
-                    "role" : "user",
-                    "content": text
-                }
-            ],
-            "stream": False
-        }
-        '''
-        body = {
-            "model": "llama3",
-            "prompt": text,
-            "stream": False
-        }
-        response = requests.post(generateUrl, json=body)
-        response_data = json.loads(response.text)
-        print("FOR ME!")
-        # content = response_data["message"]["content"]
-        content = response_data["response"]
-        #client.chat_postMessage(channel=channel,text=content)
-        #client.slack_client.chat_update(channel=channel,ts=botMessage.get('ts'), text=content)
-        client.slack_client.chat_postMessage(channel=channel,text=content)
+    message_ts = event.get('ts')
+    user_guid = event.get('user')
+    user = messaging_parser.get_real_name(message, client)
+    if "@"+client.bot in text and user_guid != client.bot and client.check_if_can_send():
+        print("Bot GUID:" + client.bot)
+        print("Username: " + user)
+        print("Message: " + text)
+        print("Message timestamp: " + message_ts)
+        print(message)
+        client.send_message()
+        generic_answer = "Generating reply for " + user + " message."
+        client.slack_client.chat_postMessage(channel=channel,text=generic_answer)
+        text = text.replace('<@' + client.bot + '>', '' + user + ': ')
+        response_count = 0
+        while response_count <= max_responses:
+            '''
+            body = {
+                "model": "llama3",
+                "messages": [
+                    {
+                        "role" : "user",
+                        "content": text
+                    }
+                ],
+                "stream": False
+            }
+            '''
+            body = {
+                "model": "llama3",
+                "prompt": text,
+                "stream": False
+            }
+            response = requests.post(generateUrl, json=body)
+            response_data = json.loads(response.text)
+            print("FOR ME!")
+            # content = response_data["message"]["content"]
+            content = response_data["response"]
+            #client.chat_postMessage(channel=channel,text=content)
+            #client.slack_client.chat_update(channel=channel,ts=botMessage.get('ts'), text=content)
+            client.slack_client.chat_postMessage(channel=channel,text=content, thread_ts=message_ts)
+            client.post_sending()
+            response_count += 1
+
 
 app.run(host='0.0.0.0', port=5000, debug=True)
