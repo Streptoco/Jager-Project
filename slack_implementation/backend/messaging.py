dc001c04e89f46572f6358a3d197a9f6e465d0e1
afikat123 <afikat154@gmail.com>
2024-05-26 19:01:51 +0300

    the bot working as expected. sending only 1 message. add counter of message in the SlackClient instance that ensure only 1 message will be sent on every tag

--- slack_implementation/backend/messaging.py
+++ slack_implementation/backend/messaging.py
@@ -19,7 +19,6 @@ client = SlackClient()
 # local ollama instance
 chatUrl = 'http://localhost:11434/api/chat'
 generateUrl = 'http://localhost:11434/api/generate'
-max_responses = 1
 
 @eventAdapter.on('message')
 def onMessage(message):
@@ -36,38 +35,33 @@ def onMessage(message):
         print("Message timestamp: " + message_ts)
         print(message)
         client.send_message()
-        generic_answer = "Generating reply for " + user + " message."
-        client.slack_client.chat_postMessage(channel=channel,text=generic_answer)
         text = text.replace('<@' + client.bot + '>', '' + user + ': ')
-        response_count = 0
-        while response_count <= max_responses:
-            '''
-            body = {
-                "model": "llama3",
-                "messages": [
-                    {
-                        "role" : "user",
-                        "content": text
-                    }
-                ],
-                "stream": False
-            }
-            '''
-            body = {
-                "model": "llama3",
-                "prompt": text,
-                "stream": False
-            }
-            response = requests.post(generateUrl, json=body)
-            response_data = json.loads(response.text)
-            print("FOR ME!")
-            # content = response_data["message"]["content"]
-            content = response_data["response"]
-            #client.chat_postMessage(channel=channel,text=content)
-            #client.slack_client.chat_update(channel=channel,ts=botMessage.get('ts'), text=content)
-            client.slack_client.chat_postMessage(channel=channel,text=content, thread_ts=message_ts)
-            client.post_sending()
-            response_count += 1
+        '''
+        body = {
+            "model": "llama3",
+            "messages": [
+                {
+                    "role" : "user",
+                    "content": text
+                }
+            ],
+            "stream": False
+        }
+        '''
+        body = {
+            "model": "llama3",
+            "prompt": text,
+            "stream": False
+        }
+        response = requests.post(generateUrl, json=body)
+        response_data = json.loads(response.text)
+        print("FOR ME!")
+        # content = response_data["message"]["content"]
+        content = response_data["response"]
+        #client.chat_postMessage(channel=channel,text=content)
+        #client.slack_client.chat_update(channel=channel,ts=botMessage.get('ts'), text=content)
+        client.slack_client.chat_postMessage(channel=channel,text=content, thread_ts=message_ts)
+        client.post_sending()
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
