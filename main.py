4288db1aa1d75671e2a0f2ab542640013350a19f
afikat123 <afikat154@gmail.com>
2024-05-30 18:54:44 +0300

    change the way of communication with the model

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def print_hello():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
     #app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('cert.crt', 'key.key'))
-    # app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('fullchain.pem', 'privkey.pem'))
+    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=('fullchain.pem', 'privkey.pem'))
