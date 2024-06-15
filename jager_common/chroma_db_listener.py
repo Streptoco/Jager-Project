989232d3e251f33daad74cb4860f0de9699747e3
afikat123 <afikat154@gmail.com>
2024-06-15 18:07:08 +0300

    testing integration to slack

--- jager_common/chroma_db_listener.py
+++ jager_common/chroma_db_listener.py
@@ -40,14 +40,15 @@ def query_db():
         query_embeddings=[embedded_prompt["embedding"]],
         n_results=5
     )
-    data = results['documents'][0][0]
+    #data = results['documents'][0][0]
+    data = [result[0] for result in results['documents']]
     # Need to be replaced with and http request to the GPU Cluster if possible
     print("the question asked: ", prompt)
     print("the data we use is ", data)
     output = ollama.generate(
         model="llama3",
         prompt=f"You are a slack assistant named Jager, your purpose is to help us search the history of our "
-               f"conversations but you don't need to mention this. Using this data only: {data}. Respond to this prompt: {prompt}"
+               f"conversations but you don't need to mention this. Using this data only: {', '.join(data)}. Respond to this prompt: {prompt}"
     )
     return jsonify(output['response'])
 
