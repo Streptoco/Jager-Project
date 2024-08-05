af9491529ee5545fa5ae3721b37b5c7aa45d72fb
afikat123 <afikat154@gmail.com>
2024-08-05 17:26:15 +0300

    switch to the bigger model

--- jager_common/ollama_ask.py
+++ jager_common/ollama_ask.py
@@ -3,6 +3,7 @@ import ollama
 
 def main(prompt):
     response = ollama.generate('llama3', prompt)
+    response = ollama.generate('llama3:70b', prompt)
     content = response['response']
     print("the Bot answer is:\n")
     print(content)
